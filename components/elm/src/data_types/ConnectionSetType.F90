5a57c494f647559a6a02e51624a3383bb4b9d74d
qhgogogo <qhgogogo@gmail.com>
2021-12-05 10:54:48 -0800

    Update ConnectionSetType.F90

--- components/elm/src/data_types/ConnectionSetType.F90
+++ components/elm/src/data_types/ConnectionSetType.F90
@@ -1,5 +1,6 @@
 module Connection_module
 
+use shr_kind_mod   , only : r8 => shr_kind_r8
 implicit none
 save
 public
@@ -7,15 +8,36 @@ public
 type, public :: connection_set_type
     Integer, pointer :: id_up(:)      ! list of ids of upwind cells
     Integer, pointer :: id_dn(:)      ! list of ids of downwind cells
-    Real, pointer :: dist(:)    ! list of distance vectors
+    Real(r8), pointer :: dist(:)    ! list of distance vectors
     Real, pointer :: area(:)      ! list of areas of faces normal to distance vectors
+contains
+    procedure, public :: connect => col_connect
 end type connection_set_type
 
 public :: get_natveg_column_id 
  
+type (connection_set_type), public, target :: conn   ! connection type
+
 contains
 
 ! ************************************************************************** !
+subroutine col_connect(this, begc, endc)
+
+   Integer, intent(in) begc, endc
+   Integer, nconn
+   class(connection_set_type) :: this  
+   allocate(this%grid_id_up(nconn)) ;  this%grid_id_up(:) = nan
+   allocate(this%grid_id_dn(nconn)) ;  this%grid_id_dn(:) = nan
+   allocate(this%area(nconn))       ;  this%area(:) = nan
+   allocate(this%dist(nconn))       ;  this%dist(:) = nan
+   do iconn = 1,nconn
+     g = begg(iconn)
+     conn%grid_id_up(iconn) = g    !... Step-2: Eventually will need to read from surface dataset
+     conn%grid_id_dn(iconn) = g+1  !...         There is already some code that we will be able to
+                                !            use to fill this data structure
+   enddo
+end subroutine col_connect
+
 function get_natveg_column_id(id) result(id_out)
 
   implicit none
@@ -25,5 +47,6 @@ function get_natveg_column_id(id) result(id_out)
 
 
 end function get_natveg_column_id
-
 end module Connection_module
+
+
