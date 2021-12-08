9c53e99d9dc0893774fe0a3c3dc1ae5c772b9375
qhgogogo <qhgogogo@gmail.com>
2021-12-08 04:11:15 -0600

    Update ConnectionSetType.F90

--- components/elm/src/data_types/ConnectionSetType.F90
+++ components/elm/src/data_types/ConnectionSetType.F90
@@ -27,26 +27,26 @@ subroutine col_connect_init(this, bounds)
 
    type(bounds_type), intent(in)    :: bounds
    class(connection_set_type)       :: this 
-   Integer                          :: nconn, iconn,begc,endc,begg, endg  
+   Integer                          :: nconn, iconn,begc(:),endc(:),begg(:), endg(:)  
    Integer                          :: dx, dz, g
    begc = bounds%begc;  endc = bounds%endc!
    begg = bounds%begg;  endg = bounds%endg
    nconn = endc-begc                                           ! number of connections in each layer
    
-   allocate(this%grid_id_up(nconn)) ;  this%grid_id_up(:) = nan
-   allocate(this%grid_id_dn(nconn)) ;  this%grid_id_dn(:) = nan
-   allocate(this%area(nconn))       ;  this%area(:) = nan
-   allocate(this%dist(nconn))       ;  this%dist(:) = nan
+   allocate(this%grid_id_up(nconn)) ;  this%grid_id_up(:) = 0  !nan not working use huge(1)?
+   allocate(this%grid_id_dn(nconn)) ;  this%grid_id_dn(:) = 0
+   allocate(this%area(nconn))       ;  this%area(:) = 0   !nan
+   allocate(this%dist(nconn))       ;  this%dist(:) = 0   !nan 
    dx = 1000  ! temporary
    dz = 1000  ! temporary
    do iconn = 1,nconn
-     g = bounds%begg(iconn)
+     g = begg(iconn)
      this%grid_id_up(iconn) = g    !  Step-2: Eventually will need to read from surface dataset
      this%grid_id_dn(iconn) = g+1  !  There is already some code that we will be able to
      this%area(iconn)       = 1000     !  use to fill this data structure, temporary value now
      this%dist(iconn)       = 1000     ! triangle law, temporary value now
    enddo
-end subroutine col_connect
+end subroutine col_connect_init
 
   function get_natveg_column_id(id) result(id_out)
 
